63c79497310527f4989b7328fde6276be2848ab7
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-09-23 11:32:53 +0900

    Should be able to check non-linked stations.
    接続していないことを判断できる

--- tests.py
+++ tests.py
@@ -4,7 +4,9 @@ import route
 def test_able_to_access():
     yokohama = route.Station("横浜")
     ohmiya = route.Station("大宮")
+    ohshima = route.Station("大島")
 
     router = route.Router()
     router.cross_link(yokohama, ohmiya)
     assert router.is_linked(yokohama, ohmiya)
+    assert not router.is_linked(yokohama, ohshima)
